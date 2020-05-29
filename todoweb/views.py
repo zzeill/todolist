7b65606d8b465edac3a6e4c0ce080c17bea215c0
Victoria <victoria.sosa@uabc.edu.mx>
2020-05-29 02:08:39 -0700

    Plantilla base y ya se muestran las listas al inicio

--- todoweb/views.py
+++ todoweb/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import List, Activity
+from django.shortcuts import render, get_object_or_404
+#from .forms import PostForm, CommentForm, RegisterForm
+from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+def lists(request):
+    lists = List.objects.order_by('-id')
+    return render(request, 'todoweb/lists.html', {'lists' : lists})
