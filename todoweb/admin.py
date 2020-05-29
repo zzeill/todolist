3299d019382bef7a13406ca9cf54ffc4d5094f49
Carlo Vega <vega.carlo@uabc.edu.mx>
2020-05-29 00:32:41 -0700

    Creación de proyecto y modelos

---
+++ todoweb/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import List, Activity
+
+admin.site.register(List)
+
+admin.site.register(Activity)
---
+++ todoweb/models.py
@@ -0,0 +1,28 @@
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
+
+class List(models.Model):
+    idUser = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+
+    def list_activities(self):
+        return self.activities.filter(done=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Activity(models.Model):
+    idList = models.ForeignKey('todoweb.List', related_name='activities', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    done = models.BooleanField(default=False)
+
+
+    def complete(self):
+        self.done = True
+        self.save()
+
+    def __str__(self):
+        return self.title
